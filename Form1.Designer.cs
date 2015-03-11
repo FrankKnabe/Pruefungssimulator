5437b72aff8ac882a21edee941b630f437ce3189
FrankKnabe <quatschmicro@web.de>
2015-03-11 11:37:32 +0100

    Signed-off-by: FrankKnabe <quatschmicro@web.de>

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.cmdBegin = new System.Windows.Forms.Button();
             this.cmdEnd2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrueflingsID = new System.Windows.Forms.TextBox();
+            this.cmdAnmeldung = new System.Windows.Forms.Button();
+            this.lblGreetings = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // cmdBegin
@@ -43,6 +51,7 @@
             this.cmdBegin.TabIndex = 0;
             this.cmdBegin.Text = "Start";
             this.cmdBegin.UseVisualStyleBackColor = true;
+            this.cmdBegin.Visible = false;
             this.cmdBegin.Click += new System.EventHandler(this.cmdBegin_Click);
             // 
             // cmdEnd2
@@ -52,16 +61,91 @@
             this.cmdEnd2.Location = new System.Drawing.Point(566, 272);
             this.cmdEnd2.Name = "cmdEnd2";
             this.cmdEnd2.Size = new System.Drawing.Size(115, 47);
-            this.cmdEnd2.TabIndex = 4;
+            this.cmdEnd2.TabIndex = 5;
             this.cmdEnd2.Text = "Beenden";
             this.cmdEnd2.UseVisualStyleBackColor = true;
             this.cmdEnd2.Click += new System.EventHandler(this.cmdEnd2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Vorname";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(89, 89);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(175, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Prüflings-ID";
+            // 
+            // txtPrueflingsID
+            // 
+            this.txtPrueflingsID.Location = new System.Drawing.Point(89, 121);
+            this.txtPrueflingsID.Name = "txtPrueflingsID";
+            this.txtPrueflingsID.Size = new System.Drawing.Size(175, 20);
+            this.txtPrueflingsID.TabIndex = 3;
+            // 
+            // cmdAnmeldung
+            // 
+            this.cmdAnmeldung.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAnmeldung.Location = new System.Drawing.Point(89, 160);
+            this.cmdAnmeldung.Name = "cmdAnmeldung";
+            this.cmdAnmeldung.Size = new System.Drawing.Size(116, 42);
+            this.cmdAnmeldung.TabIndex = 4;
+            this.cmdAnmeldung.Text = "Anmeldung";
+            this.cmdAnmeldung.UseVisualStyleBackColor = true;
+            this.cmdAnmeldung.Click += new System.EventHandler(this.cmdAnmeldung_Click);
+            // 
+            // lblGreetings
+            // 
+            this.lblGreetings.AutoSize = true;
+            this.lblGreetings.Location = new System.Drawing.Point(41, 249);
+            this.lblGreetings.Name = "lblGreetings";
+            this.lblGreetings.Size = new System.Drawing.Size(0, 13);
+            this.lblGreetings.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nachname";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(336, 89);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(175, 20);
+            this.txtSurname.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(688, 433);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblGreetings);
+            this.Controls.Add(this.cmdAnmeldung);
+            this.Controls.Add(this.txtPrueflingsID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cmdEnd2);
             this.Controls.Add(this.cmdBegin);
             this.MaximizeBox = false;
@@ -78,6 +162,14 @@
 
         private System.Windows.Forms.Button cmdBegin;
         private System.Windows.Forms.Button cmdEnd2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrueflingsID;
+        private System.Windows.Forms.Button cmdAnmeldung;
+        private System.Windows.Forms.Label lblGreetings;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSurname;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
+using System.Data.OleDb;
 
 namespace PrüfungsSimulator
 {
@@ -27,6 +28,46 @@ namespace PrüfungsSimulator
         private void cmdEnd2_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void cmdAnmeldung_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection();
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataReader reader;
+
+            con.ConnectionString = "Provider=sqloledb; Data Source=192.168.39.130; Initial Catalog=Knabe; User ID = Knabe; Password=User2016;";
+
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Prüfling where Name = '" + txtName.Text + 
+                 "' and Nachname = '" + txtSurname.Text + "' and PrüflingsID = '" + txtPrueflingsID.Text + "'";
+
+            string name = "";
+            string surname = "";
+            string id = "";
+            try
+            {
+                con.Open();
+                reader = cmd.ExecuteReader();
+                reader.Read();
+
+                name = reader["Name"].ToString();
+                surname = reader["Nachname"].ToString();
+                id = reader["PrüflingsID"].ToString();
+
+                if (name != "" && id != "" && surname != "")
+                {
+                    cmdBegin.Visible = true;
+                    lblGreetings.Text += "Hallo " + name + " " + surname;
+                }
+
+                reader.Close();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
         }
                
     }
